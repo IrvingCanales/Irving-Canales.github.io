00a66eb6082c4259ec05c638691c06716bd259aa
Irving Canales <irving@MacBook-Pro-de-Irving.local>
2020-10-06 07:13:19 -0600

    First commit portfolio react

---
+++ precache-manifest.b7e132bacf76185a6ba64b00c56982a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "162f722a5568ef51d53f31ce33e93d44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d4b3f1bbebe141e5d70",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f29ece731f21b2276a11",
+    "url": "/static/js/2.e4e08dad.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.e4e08dad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d4b3f1bbebe141e5d70",
+    "url": "/static/js/main.8c03b5e7.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
